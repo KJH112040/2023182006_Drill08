7a95d9495097f31067f66f0952ffa04cc3cf1801
KJH04_ <10wjdgp04@gmail.com>
2024-10-28 15:06:57 +0900

    실습 내용

--- boy.py
+++ boy.py
@@ -1,17 +1,54 @@
-from pico2d import load_image
-from state_machine import StateMachine
+from pico2d import load_image, get_time
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE
+
+from state_machine import StateMachine, time_out, space_down
+
+
+class Boy:
+    image = None
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.dir = 0
+        self.action = 3
+        if Boy.image==None:
+            self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine(self) #소년 객체의 state machine 생성
+        self.state_machine.start(Sleep) #초기 상태가 Idle
+        self.state_machine.set_transitions(
+            {
+                Idle  : { time_out : Sleep },
+                Sleep : { space_down : Idle }
+            }
+        )
+
+    def update(self):
+        self.state_machine.undate()
+
+    def handle_event(self, event):
+        # event : 입력 이벤트 key mouse
+        # 우리가 state machine에게 전달해줄껀 ( , )
+        self.state_machine.add_event(('INPUT',event))
+        pass
+
+    def draw(self):
+        self.state_machine.draw()
 
 #상태를 쿨래스를 통해서 정의함.
 class Idle:
     @staticmethod   #@는 데코레이드 같은 기능
-    def enter():
+    def enter(boy):
+        # 현재 시간을 저장
+        boy.start_time = get_time()
         pass
     @staticmethod
-    def exit():
+    def exit(boy):
         pass
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1)%8
+        if get_time() - boy.start_time > 3:
+            boy.state_machine.add_event(('TIME_OUT', 0))
         pass
     @staticmethod
     def draw(boy):
@@ -20,10 +57,10 @@ class Idle:
 
 class Sleep:
     @staticmethod
-    def enter():
+    def enter(boy):
         pass
     @staticmethod
-    def exit():
+    def exit(boy):
         pass
     @staticmethod
     def do(boy):
@@ -37,26 +74,3 @@ class Sleep:
             boy.x - 25,boy.y - 25,100,100
         )
         pass
-
-class Boy:
-    image = None
-    def __init__(self):
-        self.x, self.y = 400, 90
-        self.frame = 0
-        self.dir = 0
-        self.action = 3
-        if Boy.image==None:
-            self.image = load_image('animation_sheet.png')
-        self.state_machine = StateMachine(self) #소년 객체의 state machine 생성
-        self.state_machine.start(Sleep) #초기 상태가 Idle
-
-    def update(self):
-        self.state_machine.undate()
-        # self.frame = (self.frame + 1) % 8
-
-    def handle_event(self, event):
-        pass
-
-    def draw(self):
-        self.state_machine.draw()
-        #self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
--- state_machine.py
+++ state_machine.py
@@ -1,12 +1,47 @@
+# 이벤트 체크 함수 정의
+# 상태 이벤트 e = (종류, 실제값) 튜플로 정의
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE
+
+
+def space_down(e): #e가 space down인지 판단? True or False
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key==SDLK_SPACE
+
+def time_out(e): #e가 time out 인지 판단?
+    return e[0] == 'TIME_OUT'
+
 class StateMachine:
     def __init__(self,obj):
         self.obj = obj # 어떤 객체를 위한 상태 머신인지 알려줌.
+        # 상태 이벤트를 보관할 리스트
+        self.event_q = []
 
     def start(self,state):
         self.cur_state = state #시작 상태를 받아서, 그걸로 현재 상태를 정의.
 
     def undate(self):
         self.cur_state.do(self.obj) #Idle.do()
+        # 혹시 이벤트가 있나?
+        if self.event_q: #list는 멤버가 있으면 True
+            e = self.event_q.pop(0)
+            # 이시점에서 우리한테 주어진 정보는?
+            # e
+            # cur_state
+            # 현재 상태와 현재 발생한 이벤트에 따라서
+            # 다음 상태를 결정하는 방법은?
+            # 상태 변환 테이블를 이용
+            for check_event, next_state in self.transitions[self.cur_state].items():
+                if check_event(e): #
+                    self.cur_state.exit(self.obj)
+                    self.cur_state = next_state
+                    self.cur_state.enter(self.obj)
 
     def draw(self):
         self.cur_state.draw(self.obj)
+
+    def add_event(self, e):
+        self.event_q.append(e)
+        pass
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
+        pass
