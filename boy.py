ecc497eb346e6fd9663bcc2b4a990b09f3c1628c
KJH04_ <10wjdgp04@gmail.com>
2024-10-29 11:13:56 +0900

    실습

--- boy.py
+++ boy.py
@@ -1,7 +1,7 @@
 from pico2d import load_image, get_time
 from sdl2 import SDL_KEYDOWN, SDLK_SPACE
 
-from state_machine import StateMachine, time_out, space_down
+from state_machine import StateMachine, time_out, space_down, right_down, left_up, left_down, right_up, start_event
 
 
 class Boy:
@@ -14,11 +14,12 @@ class Boy:
         if Boy.image==None:
             self.image = load_image('animation_sheet.png')
         self.state_machine = StateMachine(self) #소년 객체의 state machine 생성
-        self.state_machine.start(Sleep) #초기 상태가 Idle
+        self.state_machine.start(Idle) #초기 상태가 Idle
         self.state_machine.set_transitions(
             {
-                Idle  : { time_out : Sleep },
-                Sleep : { space_down : Idle }
+                Idle  : { right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out : Sleep },
+                Run : {right_down: Idle, left_down: Idle, right_up:Idle, left_up:Idle}, # Run 상태에서 어떤 이벤트가 들어와도 처리하지 않겠다.
+                Sleep : { right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down : Idle }
             }
         )
 
@@ -37,12 +38,20 @@ class Boy:
 #상태를 쿨래스를 통해서 정의함.
 class Idle:
     @staticmethod   #@는 데코레이드 같은 기능
-    def enter(boy):
+    def enter(boy,e):
+        if left_up(e) or right_down(e):
+            boy.action = 2
+            boy.face_dir = -1
+        elif right_up(e) or left_down(e) or start_event(e):
+            boy.action = 3
+            boy.face_dir = 1
+        boy.dir = 0 #정지 상태
+        boy.frame = 0
         # 현재 시간을 저장
         boy.start_time = get_time()
         pass
     @staticmethod
-    def exit(boy):
+    def exit(boy,e):
         pass
     @staticmethod
     def do(boy):
@@ -57,10 +66,10 @@ class Idle:
 
 class Sleep:
     @staticmethod
-    def enter(boy):
+    def enter(boy,e):
         pass
     @staticmethod
-    def exit(boy):
+    def exit(boy,e):
         pass
     @staticmethod
     def do(boy):
@@ -68,9 +77,35 @@ class Sleep:
         pass
     @staticmethod
     def draw(boy):
-        boy.image.clip_composite_draw(
-            boy.frame*100,300,100,100,3.141592/2, #90회전
-            '', #좌우상하 반전X
-            boy.x - 25,boy.y - 25,100,100
-        )
+        if boy.face_dir ==1: #오른쪽 바라본 상태에서 눕기
+            boy.image.clip_composite_draw(
+                boy.frame*100,300,100,100,3.141592/2, #90회전
+                '', #좌우상하 반전X
+                boy.x - 25,boy.y - 25,100,100
+            )
+        elif boy.face_dir ==-1: #왼쪽 바라본 상태에서 눕기
+            boy.image.clip_composite_draw(
+                boy.frame * 100, 200, 100, 100, -3.141592 / 2,  # 90회전
+                '',  # 좌우상하 반전X
+                boy.x + 25, boy.y - 25, 100, 100
+            )
         pass
+
+class Run:
+    @staticmethod
+    def enter(boy,e):
+        if right_down(e) or left_up(e):
+            boy.dir,boy.face_dir,boy.action=1,1,1
+        elif left_down(e) or right_up(e):
+            boy.dir,boy.face_dir,boy.action=-1,-1,0
+    @staticmethod
+    def exit(boy,e):
+        pass
+    @staticmethod
+    def do(boy):
+        boy.frame=(boy.frame+1)%8
+        boy.x += boy.dir * 5
+        pass
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame*100,boy.action*100,100,100,boy.x,boy.y)
--- state_machine.py
+++ state_machine.py
@@ -1,13 +1,26 @@
 # 이벤트 체크 함수 정의
 # 상태 이벤트 e = (종류, 실제값) 튜플로 정의
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
 
-
+def start_event(e):
+    return e[0]=='START'
 def space_down(e): #e가 space down인지 판단? True or False
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key==SDLK_SPACE
 
 def time_out(e): #e가 time out 인지 판단?
     return e[0] == 'TIME_OUT'
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key ==SDLK_RIGHT
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 class StateMachine:
     def __init__(self,obj):
@@ -17,6 +30,8 @@ class StateMachine:
 
     def start(self,state):
         self.cur_state = state #시작 상태를 받아서, 그걸로 현재 상태를 정의.
+        self.cur_state.enter(self.obj, ('START', 0))
+        print(f'Enter into {state}')
 
     def undate(self):
         self.cur_state.do(self.obj) #Idle.do()
@@ -31,14 +46,20 @@ class StateMachine:
             # 상태 변환 테이블를 이용
             for check_event, next_state in self.transitions[self.cur_state].items():
                 if check_event(e): #
-                    self.cur_state.exit(self.obj)
+                    print(f'Exit from {self.cur_state}')
+                    self.cur_state.exit(self.obj,e)
                     self.cur_state = next_state
-                    self.cur_state.enter(self.obj)
+                    print(f'Enter into {next_state}')
+                    self.cur_state.enter(self.obj,e) # 상태 변환의 이유를 명확히 알려줘.
+                    return # 제대로 이벤트에 따른 상태 변환 완료
+            #이 시점으로 왔다는 것은, event에 따른 전환 못함.
+            print(f'    WARNING: {e} not handled at state {self.cur_state}')
 
     def draw(self):
         self.cur_state.draw(self.obj)
 
     def add_event(self, e):
+        print(f'    DEBUG: add event {e}')
         self.event_q.append(e)
         pass
 
